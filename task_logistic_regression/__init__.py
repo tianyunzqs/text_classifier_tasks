339136c7f7000564145b1717929f42ed2e60b43c
zhuqisi <qszhu@fiberhome.com>
2019-03-13 18:16:17 +0800

    文本分类方法——logistics回归

---
+++ task_logistic_regression/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time        : 2019/3/13 18:08
+# @Author      : tianyunzqs
+# @Description : 
+
---
+++ task_logistic_regression/step1_data_process.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time        : 2019/3/13 18:09
+# @Author      : tianyunzqs
+# @Description : 
+
