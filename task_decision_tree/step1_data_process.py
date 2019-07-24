b05c100f2853e20972ac2f1e0ce3f3235e887c3a
zhuqisi <qszhu@fiberhome.com>
2019-07-24 10:30:10 +0800

    更新决策树文本分类

--- task_decision_tree/step1_data_process.py
+++ task_decision_tree/step1_data_process.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # @Time        : 2019/3/13 18:09
 # @Author      : tianyunzqs
-# @Description : 数据预处理
+# @Description : 数据预处理，原始数据来源：https://blog.csdn.net/m0_38088359/article/details/83004972
 
 import json
 import pickle
@@ -20,9 +20,11 @@ def load_data(path, stopwords):
     with open(path, "r", encoding="utf-8") as f:
         lines = f.readlines()
         for line in tqdm(lines):
-            line_json = json.loads(line)
-            classify = line_json["classify"]
-            text = line_json["title"] + " " + line_json["content"]
+            line = line.strip()
+            parts = line.split('\t')
+            if len(parts) != 2:
+                continue
+            classify, text = parts[0], parts[1]
             segwords = " ".join([w.word for w in pseg.cut(text) if w.word not in stopwords])
             if classify not in data:
                 data[classify] = [segwords]
@@ -31,11 +33,27 @@ def load_data(path, stopwords):
     return data
 
 
+# def load_data(path, stopwords):
+#     data = {}
+#     with open(path, "r", encoding="utf-8") as f:
+#         lines = f.readlines()
+#         for line in tqdm(lines):
+#             line_json = json.loads(line)
+#             classify = line_json["classify"]
+#             text = line_json["title"] + " " + line_json["content"]
+#             segwords = " ".join([w.word for w in pseg.cut(text) if w.word not in stopwords])
+#             if classify not in data:
+#                 data[classify] = [segwords]
+#             else:
+#                 data[classify].append(segwords)
+#     return data
+
+
 if __name__ == '__main__':
     stopwords = load_stopwords(r"../sample_data/stopwords.txt")
-    train_path = r"../sample_data/train.dat"
+    train_path = r"../sample_data/cnews.train.txt"
     train_data = load_data(train_path, stopwords=stopwords)
-    test_path = r"../sample_data/test.dat"
+    test_path = r"../sample_data/cnews.test.txt"
     test_data = load_data(test_path, stopwords=stopwords)
     pickle.dump(train_data, open("./train_data.pickle", "wb"))
     pickle.dump(test_data, open("./test_data.pickle", "wb"))
